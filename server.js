77b98e3991aab8a6d7ca2dd49277384af86bdd66
priscilla ma <priscillama621@gmail.com>
2022-02-27 16:03:35 -0500

    changed connection var to marketplace, moved everything to umm js. played around with array in server js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2');
 const inquirer = require('inquirer');
 const cTable = require('console.table');
 
-const connection = mysql.createConnection({
+const marketplace = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'rootroot',
@@ -11,3 +11,18 @@ const connection = mysql.createConnection({
     console.log('CONNECTED TO DATABASE!☆')
 );
 
+// marketplace.query(`SELECT dept_name FROM departments`,
+//                         (err, results) => {
+//                             for(let i = 0; i < results.length; i++){
+//                                 console.log(Object.values(i))
+//                             }
+//                         }
+//                     )
+
+let allDepts = [];
+marketplace.query('SELECT * FROM departments', (err, results) => {
+    for(const meow of results) {
+        // console.log(meow.dept_name);
+        allDepts.push(meow);
+    }
+});
