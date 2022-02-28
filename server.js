228b77b0b5fe30c41fe26bdbc91ebd4ecf38cd52
priscilla ma <priscillama621@gmail.com>
2022-02-27 20:02:25 -0500

    more testing things on this page

--- server.js
+++ server.js
@@ -19,10 +19,27 @@ const marketplace = mysql.createConnection({
 //                         }
 //                     )
 
-let allDepts = [];
-marketplace.query('SELECT * FROM departments', (err, results) => {
-    for(const meow of results) {
+// let allDepts = [];
+// marketplace.query('SELECT * FROM departments', (err, results) => {
+//     for(const meow of results) {
         // console.log(meow.dept_name);
-        allDepts.push(meow);
-    }
-});
+//         allDepts.push(meow);
+//     }
+// });
+
+// let allRoles = [];
+// marketplace.query('SELECT role FROM roles', (err, results) => {
+//     for(const role of results) {
+//         // console.log(role.role);
+//         allRoles.push(role.role);
+//     }
+// });
+
+// let allEmployees = [];
+// marketplace.query('SELECT CONCAT(first_name, " ", last_name) AS name FROM employees', (err, results) => {
+//     for(const employee of results) {
+//         allEmployees.push(employee.name);
+//     }
+// });
+
+console.log(marketplace.query(`SELECT employee_id FROM employees WHERE CONCAT(first_name, " ", last_name)="MAYA RUDBEKIA"`));
